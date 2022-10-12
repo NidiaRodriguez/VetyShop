000aa27b3bc53426dafa53afc46f9e45fcd05627
NidiaRodriguez <nidiarodriguezv@gmail.com>
2022-10-12 15:13:32 -0500

    clase del 7 octubre

--- back/server.js
+++ back/server.js
@@ -1,8 +1,12 @@
 const app=require("./app")
+const connectDatabase = require("./config/database");
 
 //setear el archivo de configuracion
 const dotenv=require("dotenv");
 dotenv.config({path: 'back/config/config.env'})
+
+//Configurar base de datos
+connectDatabase();
 
 //Llamemos al server
 const server=app.listen(process.env.PORT, () => {
