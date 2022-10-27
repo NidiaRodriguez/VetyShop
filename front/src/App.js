4a23178e9bcc8a19271899786f346065ce83b82a
NidiaRodriguez <nidiarodriguezv@gmail.com>
2022-10-26 20:43:57 -0500

    hasta la clase 25 oct 2022

--- front/src/App.js
+++ front/src/App.js
@@ -1,14 +1,29 @@
 import './App.css';
 import React from 'react';
+import Header from './components/layout/Header';
+import { Footer } from './components/layout/Footer';
+import Home from './components/Home';
+import { ProductDetails } from './components/products/ProductDetails';
+//Router traido desde react-router-dom (no confundir con el de express)
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 
 function App() {
   return (
+    <Router>
     <div className="App">
-        <h1>Tienda de mascotas Vety Shop</h1>
-        <img src="./images/vetyshop.png" alt="Aqui va el logo"></img>
-      </div>
-  );
+        <Header />
+        <div className='container container-fluid'>
+          <Routes>
+            <Route path="/" element={<Home />}/>
+            <Route path="/Home" element={<Home />}/>
+            <Route path="/producto/:id" element={<ProductDetails />}/>
+          </Routes>
+        </div>
+        <Footer />
+    </div>   
+    </Router>
+  ); 
 }
 
 export default App;
