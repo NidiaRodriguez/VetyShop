89c4f3e12261cbfa48917635d61358b22f1a5f37
NidiaRodriguez <nidiarodriguezv@gmail.com>
2022-11-09 10:16:52 -0500

    hasta 9 nov2022

--- front/src/store.js
+++ front/src/store.js
@@ -2,10 +2,12 @@ import { createStore, combineReducers, applyMiddleware, compose} from 'redux';
 import thunk from 'redux-thunk';
 import { composeWithDevTools} from 'redux-devtools-extension';
 import { productsReducer, productDetailsReducer } from './reducer/productReducer';
+import { authReducer } from './reducer/userReducer';
 
 const reducer= combineReducers ({
     products:productsReducer,
-    productDetails: productDetailsReducer
+    productDetails: productDetailsReducer,
+    authUser: authReducer
 })
     
 let initialState = {}
